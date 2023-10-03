5afc6d7edd1a1cfcd6c306ae913025205f47c9a5
Marek Subocz <mrksubocz@gmail.com>
2023-10-03 13:28:00 +0200

    small demo.py fix

--- demo.py
+++ demo.py
@@ -11,53 +11,57 @@ from instance_parser import (
 from partial_brute_force import solve_with_pbruteforce
 from utilities import draw_solution
 
-# if you see some excessive warnings from dwave
-# filterwarnings("ignore")
+def main(path):
+    # if you see some excessive warnings from dwave
+    # filterwarnings("ignore")
 
-jobs = readInstance(sys.argv[1])
+    jobs = readInstance(path)
 
-first_solution = solve_greedily(jobs)
-first_result = get_result(jobs, first_solution)
-print(f"Result without squashing: {first_result}")
+    first_solution = solve_greedily(jobs)
+    first_result = get_result(jobs, first_solution)
+    print(f"Result without squashing: {first_result}")
 
-# job squashing
-squashed_jobs = squash_lengths(jobs)
+    # job squashing
+    squashed_jobs = squash_lengths(jobs)
 
-# uncomment to skip job squashing
-# squashed_jobs = jobs
+    # uncomment to skip job squashing
+    # squashed_jobs = jobs
 
-order = get_order(first_solution)
-initial_solution = solve_with_order(squashed_jobs, order)
+    order = get_order(first_solution)
+    initial_solution = solve_with_order(squashed_jobs, order)
 
-# uncomment if you want to leave space between operations at the start
-initial_solution = first_solution
+    # uncomment if you want to leave space between operations at the start
+    initial_solution = first_solution
 
-initial_result = get_result(squashed_jobs, initial_solution)
-print(f"Initial (greedy) solution result: {initial_result}")
-draw_solution(squashed_jobs, initial_solution, x_max=initial_result)
+    initial_result = get_result(squashed_jobs, initial_solution)
+    print(f"Initial (greedy) solution result: {initial_result}")
+    draw_solution(squashed_jobs, initial_solution, x_max=initial_result)
 
-print("Performing the algorithm...")
+    print("Performing the algorithm...")
 
-# main loop
-last_result = initial_result
-current_solution = {}
-for current_solution, _ in solve_with_pbruteforce(squashed_jobs,
-                                                  initial_solution,
-                                                  window_size=initial_result,
-                                                  qpu=True,
-                                                  lagrange_one_hot=1,
-                                                  lagrange_precedence=2,
-                                                  lagrange_share=2,
-                                                  num_of_iterations=1):
-    current_result = get_result(squashed_jobs, current_solution)
+    # main loop
+    last_result = initial_result
+    current_solution = {}
+    for current_solution, _ in solve_with_pbruteforce(squashed_jobs,
+                                                      initial_solution,
+                                                      window_size=initial_result,
+                                                      qpu=True,
+                                                      lagrange_one_hot=1,
+                                                      lagrange_precedence=2,
+                                                      lagrange_share=2,
+                                                      num_of_iterations=1):
+        current_result = get_result(squashed_jobs, current_solution)
 
-    if current_result < last_result:
-        last_result = current_result
-        draw_solution(squashed_jobs, current_solution, x_max=initial_result)
+        if current_result < last_result:
+            last_result = current_result
+            draw_solution(squashed_jobs, current_solution, x_max=initial_result)
 
-    print(f"Current_result: {get_result(squashed_jobs, current_solution)}")
+        print(f"Current_result: {get_result(squashed_jobs, current_solution)}")
 
-# Using the order of new solution to solve the problem with full-time jobs
-order = get_order(current_solution)
-print("Streching jobs to full length...")
-print("final result: ", get_result(jobs, solve_with_order(jobs, order)))
+    # Using the order of new solution to solve the problem with full-time jobs
+    order = get_order(current_solution)
+    print("Streching jobs to full length...")
+    print("final result: ", get_result(jobs, solve_with_order(jobs, order)))
+
+if __name__ == '__main__':
+    main(sys.argv[1])
